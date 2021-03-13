1534a1e0fabf9dedaa0e20b3a222fab4b63923e4
Alain Rusnov <alainrusnov@gmail.com>
2021-03-13 14:14:12 -0500

    box rot - inter

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 
 =======
 # pavillon
->>>>>>> 8f1d12cb558e3f78089ca8b07d2b3c3c737014bb
+// React-three-Fiber proto
+
+// controls in - boxes interactive
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from 'react-three-fiber'
+import { Canvas, extend, useThree, useFrame } from 'react-three-fiber'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function Box(props) {
   // This reference will give us direct access to the mesh
@@ -20,19 +21,46 @@ function Box(props) {
       onPointerOver={(e) => setHover(true)}
       onPointerOut={(e) => setHover(false)}>
       <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'aqua'} />
     </mesh>
   )
 }
 
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  // Get a reference to the Three.js Camera, and the canvas html element.
+  // We need these to setup the OrbitControls class.
+  // https://threejs.org/docs/#examples/en/controls/OrbitControls
+
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
+  // Ref to the controls, so that we can update them on every frame with useFrame
+  const controls = useRef();
+  useFrame(() => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      autoRotate={true}
+      enableZoom={false}
+    />
+  );
+};
+
 export default function App() {
   return (
     <Canvas>
+      <CameraControls />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
+      <Box position={[1.2, 5, -5]} />
     </Canvas>
   )
 }
